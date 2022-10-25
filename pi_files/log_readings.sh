22c3e7cd01f8cda71df693c242888695a365b80d
RJWorth <bot.collins@gmail.com>
2022-10-25 05:10:36 +0100

    updating file path

--- pi_files/log_readings.sh
+++ pi_files/log_readings.sh
@@ -1,1 +1,1 @@
-/usr/bin/python3.7 ~/Documents/read_soil_N.py >> ~/Documents/soil_sensor_readings.csv
+/usr/bin/python3.7 ~/Code/RosemarySurvival/pi_files/read_soil_N.py >> ~/Code/RosemarySurvival/pi_files/soil_sensor_readings.csv
